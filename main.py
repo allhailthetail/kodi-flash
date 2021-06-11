17116701890bc2dbe78bf8bdf73ca9dd9d324eb8
allhailthetail <matthewyounger33@gmail.com>
2021-06-11 12:26:40 -0500

    Update main.py
    Improved functionality.  Now fully automated in Linux using venv and the requirements.txt file to satisfy requirements.

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 #
 #
 # main.py 
-# Codium Flash Utility.
+# Codium(R) Flash Utility.
 #
 #
 
 # import necessary python modules:
 from webbot import Browser
+import pyautogui
 import time
-
-# these modules should be deprecated:
-#import selenium
-#from selenium import webdriver
 
 # Static values that are required to execute the program:
 #
 IP = '192.168.0.1'
-USERNAME = 'USER'
-PASSWORD = 'PASSWORD' 
-UPGRADE_FILE = ''
-TARFILE = ''
-CERTPAIR_LOC = ''
-CONFIG_LOC = ''
+USERNAME = '#USERNAME'
+PASSWORD = '#PASSWORD' 
+UPGRADE_FILE = '#FILE'
+TARFILE = '#FILE'
+CERTPAIR_LOC = '#FILE'
+CONFIG_LOC = '#FILE'
 
 #
 #
@@ -53,14 +50,17 @@ def add_certs_cpe(sess, ip, ca_cert, certpair_loc):
     sess.type(f"{ca_cert}", id="ca_file")
     sess.type(f"{pem_loc}", id="cert_file")
     sess.type(f"{key_loc}", id="key_file")
-    sess.click("Upload", tag="span")
+    sess.click("Upload", tag="span") 
+    time.sleep(30)
+    pyautogui.press('enter')
 
 def upload_config_cpe(sess, ip, conf):
     sess.go_to(f"https://{ip}/maintenance.asp")
     time.sleep(5)
     sess.type(f"{conf}", id="file")
-    sess.click("Upload", id="span")
-
+    sess.click("Upload", tag="span")
+    time.sleep(30)
+    pyautogui.press('enter')
 #
 #
 # End Function Definitions
@@ -74,38 +74,30 @@ def upload_config_cpe(sess, ip, conf):
 #
 
 def main():
+    # This is a basic example of the program running:
+    # in a nutshell, the radio gets firmware flashed,
+    # certificates added, and a custom config uploaded.  
+    # The idea behind the example is to illustrate taking a radio from 
+    # out-of-box condition to production-ready.
+    #
     # Initiate main browser session:
     main_session = login_cpe(IP, USERNAME, PASSWORD) 
-    
+    # Wait for main page to load:    
     time.sleep(5)
-
     # Example, Upgrade CPE:
     upgrade_cpe(main_session, IP, UPGRADE_FILE)
-    
-    #input("Upgrade Complete, Press Enter or Exit")
-
     # Wait for reboot post-upgrade:
-    time.sleep(80)
-
+    time.sleep(60)
     # NOTE system reboots after update, login required:
-    # to do: make this nicer.  Close/re-open wrapper?
     main_session = login_cpe(IP, USERNAME, PASSWORD) 
-
     # Example, add certificates:
     add_certs_cpe(main_session, IP, TARFILE, CERTPAIR_LOC)
-    
-    # to add: close wrapper and re-open, to avoid pop-up crash that verifies successful upload!
-    # to add: remove input stop-point, instead wait for pop up, the check for confirmation of successful cert upload. 
-    input("Certificates Complete, Press Enter or Exit")
-
     # Example, add configuration:
     upload_config_cpe(main_session, IP, CONFIG_LOC)
-    
-    print("Config Uploaded Successfully!")
 
     # Prompt to end the program.  
     input('press enter to exit program successfully.')
-    
+
 # 
 #
 # End Main Runtime Definition
